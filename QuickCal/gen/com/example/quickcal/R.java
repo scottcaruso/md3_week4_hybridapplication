fd5220df610273a66a5fdc0363cf46c9080d728b
Scott Caruso <scottcaruso@fullsail.edu>
2013-09-24 23:13:41 -0400

    Created basic webview in main page.

--- QuickCal/gen/com/example/quickcal/R.java
+++ QuickCal/gen/com/example/quickcal/R.java
@@ -24,13 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int webview=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_web_view=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int web_view=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
