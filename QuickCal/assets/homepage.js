33fee26b9f78dcdb8575313f3513f7305e410f2b
Scott Caruso <scottcaruso@fullsail.edu>
2013-09-25 23:15:42 -0400

    Basic Android handling.

--- QuickCal/assets/homepage.js
+++ QuickCal/assets/homepage.js
@@ -4,17 +4,6 @@
 
 //The below function gets the name of elements from the form.
 
-/*$(document).bind("pageinit", function(){
-   var form = $("#addcardform");
-   form.validate({
-      invalidHandler: function(form, validator){},
-      submitHandler: function(){
-         saveCard();
-      }
-   });
-});*/
-
-
 function elementName(x){
    var elementName = document.getElementById(x);
    return elementName;              
@@ -35,30 +24,8 @@ function enableDatePickers(){
       elementName("starttime").disabled = true;
       elementName("endtime").disabled = true;
    }
-}
-
-/*   if(elementName("submit").value != "Edit Card"){
-      var y = localStorage.length;
-      var id = y+1;
-   } else {
-      var id = elementName("submit").key;
-      };
-   var cardColors = getCardColors();
-   var cardType = getCardType();
-   var card = {};
-      card.name = ["Card Name:", elementName("cardname").value];
-      card.usage = ["Currently In Use?", elementName("currentuse").value];
-      card.type = ["Card Type:", cardType];
-      card.mana = ["Mana Cost:", elementName("manacost").value];
-      card.colors = ["Colors:", cardColors];
-      card.notes = ["Notes:", elementName("comments").value];
-      card.number = ["Number Owned:", elementName("numberowned").value];
-   localStorage.setItem(id, JSON.stringify(card));
-   alert(elementName("cardname").value + " has been added!");
-   window.location="#home";
-   window.location.reload();
-};*/
+};
 
 //Make things happen when the links are clicked.
 var testClick = elementName("submit");
-testClick.addEventListener("click", testFunction);
+testClick.addEventListener("click", Native.showToast("Hello!"));
